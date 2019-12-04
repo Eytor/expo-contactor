987d48bf439e2f6f73fd38c6404ebb462f2a2591
Jón Bjarni Ólafsson <jon.bjarni.olafsson@advania.is>
2019-12-04 11:28:55 +0000

    Final touches to contactscreen and filterlement

--- src/screens/ContactScreen/ContactScreen.styles.js
+++ src/screens/ContactScreen/ContactScreen.styles.js
@@ -2,27 +2,6 @@ import { StyleSheet } from 'react-native';
 import { Colors } from '../../resources/resources';
 
 export default StyleSheet.create({
-    flatlistItem: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        paddingVertical: 5,
-        borderWidth: 0,
-        borderBottomWidth: 1,
-        borderColor: Colors.arsenic,
-    },
-    image: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        marginRight: 15,
-    },
-    itemName: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: '500',
-    },
-
     icon: {
         width: 25,
         height: 25,
